136d848bce935320a71aa0e142cb4206888d3052
Daniel Acosta <adanispro@gmail.com>
2025-01-18 09:48:40 -0500

    CreacionProyecto

---
+++ src/test/java/com/aluracursos/ChallengeForoHub/ChallengeForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ChallengeForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
